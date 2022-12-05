ab25a41fcc1f59f8dfec3590d273057902444753
Daniel Ben <contato.bendaniel@gmail.com>
2022-12-05 02:22:57 -0400

    melhora estilizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import './App.css';
+import { getDatabase, ref as dbRef, set } from "firebase/database";
+import { app } from './firebaseInit'
 import Carrossel from './Carrossel';
+import './App.css';
 
 function App() {
+
+  function handlePageLoad() {
+
+  }
+
+
   return (
     <div className="App">
-      <Carrossel title={'Arvore'} />
+      <Carrossel carrossel={test} />
+      <Carrossel carrossel={test} />
+      <Carrossel carrossel={test} />
+      <Carrossel carrossel={test} />
+      <Carrossel carrossel={test} />
     </div>
   );
 }
 
 export default App;
+
+
+const test = {
+  title: 'Livros lidos esse ano',
+  items: [
+    {
+      description: 'Em O idiota, Dostoiévski constrói um dos personagens mais impressionantes de toda a literatura mundial - o humanista e epiléptico príncipe Míchkin',
+      imageUrl: 'https://a-static.mlcdn.com.br/800x560/livro-o-idiota/magazineluiza/223741200/cdabecb3199184ad9762fbbf307540c9.jpg'
+    }, {
+      description: 'Introdução à lingugagem Typescript',
+      imageUrl: 'https://m.media-amazon.com/images/I/41JHd+anIkL._SY346_.jpg',
+    }, {
+      description: 'Gente pobre mostra ao leitor a dura realidade vivida pelos moradores de São Petesburgo, no século XIX por meio das cartas trocadas entre os protagonistas.',
+      imageUrl: 'https://m.media-amazon.com/images/I/51-vqFZS1cL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    }, {
+      description: 'Em um mundo onde as pessoas vivem em função das telas e a literatura está ameaçada de extinção, os livros são objetos proibidos, e seus portadores são considerados criminosos.',
+      imageUrl: 'https://m.media-amazon.com/images/I/41PVVpQf-sL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    }, {
+      description: 'Curso prático ensinando SQL',
+      imageUrl: 'https://m.media-amazon.com/images/I/51Meg8yMu8L._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    }, {
+      description: 'Contada em sua famosa prosa poderosa e minimalista, esta história de coragem e triunfo pessoal é até hoje uma das obras mais duradouras de Ernest Hemingway.',
+      imageUrl: 'https://m.media-amazon.com/images/I/419Jl7oflOL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+    },
+  ]
+}
--- src/Carrossel/index.tsx
+++ src/Carrossel/index.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './style.css'
-import { app } from '../firebaseInit'
-import { getDatabase } from "firebase/database";
 
 type mouseEvent = React.MouseEvent<HTMLImageElement>
 type touchEvent = React.TouchEvent<HTMLImageElement>
 interface iCarrossel {
-    title: String;
+    carrossel: {
+        title: string;
+        items: {
+            description: string;
+            imageUrl: string;
+        }[];
+    }
 }
-export default function Carrossel({ title }: iCarrossel) {
-    const database = getDatabase(app);
-
+export default function Carrossel({ carrossel }: iCarrossel) {
     const [isDragging, setIsDragging] = useState(false);
     const [startPosition, setStartPosition] = useState(0);
     const [currentPosition, setCurrentPosition] = useState(0);
@@ -40,36 +42,34 @@ export default function Carrossel({ title }: iCarrossel) {
 
     return (
         <div className='carrossel__container'>
-            <h2 className='carrossel__title'>{title}</h2>
-            <div className='carrossel__slider-container'>
-                <div className='carrossel__slider'
-                    style={{ transform: `translateX(${currentTranslate}px)` }}
-                    data-slider
-                >
-                    {test.map((image, index) => (
-                        <div key={index}
-                            className='slider__item'
-                            data-item >
-                            <img
-                                className='slider__img'
-                                src={image.imageUrl}
-                                alt=''
-                                onDragStart={preventDragImageEvent}
-                                onTouchStart={(event) => touchStart(event, index)}
-                                onMouseDown={(event) => touchStart(event, index)}
+            <h2 className='carrossel__title'>{carrossel.title}</h2>
+            <div className='carrossel__slider'
+                style={{ transform: `translateX(${currentTranslate}px)` }}
+                data-slider
+            >
+                {carrossel.items.map((item, index) => (
+                    <div key={index}
+                        className='slider__item'
+                        data-item >
+                        <img
+                            className='slider__img'
+                            src={item.imageUrl}
+                            alt=''
+                            onDragStart={preventDragImageEvent}
+                            onTouchStart={(event) => touchStart(event, index)}
+                            onMouseDown={(event) => touchStart(event, index)}
 
-                                onTouchMove={(event) => touchMove(event)}
-                                onMouseMove={(event) => touchMove(event)}
+                            onTouchMove={(event) => touchMove(event)}
+                            onMouseMove={(event) => touchMove(event)}
 
-                                onTouchEnd={() => touchEnd()}
-                                onMouseUp={() => touchEnd()}
-                                onMouseLeave={() => touchEnd()}
-                            />
-                            <p className='slider__description'>{image.description}</p>
-                        </div>
-                    )
-                    )}
-                </div>
+                            onTouchEnd={() => touchEnd()}
+                            onMouseUp={() => touchEnd()}
+                            onMouseLeave={() => touchEnd()}
+                        />
+                        <p className='slider__description'>{item.description}</p>
+                    </div>
+                )
+                )}
             </div>
         </div>
 
@@ -91,29 +91,3 @@ function isTooFar(): boolean {
     console.log(slider?.clientWidth)
     return false;
 }
-
-// random picture
-const test = [
-    {
-        description: 'descrição',
-        imageUrl: 'arvore1.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore2.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore3.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore4.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore4.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore4.jpeg',
-    }, {
-        description: 'descrição',
-        imageUrl: 'arvore4.jpeg',
-    },
-]
