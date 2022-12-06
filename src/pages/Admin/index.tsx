fd73f3c2b9ea5a6fbf2c64cb28a5f13b25da02de
Daniel Ben <contato.bendaniel@gmail.com>
2022-12-06 04:50:14 -0400

    add responsividade a page admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -4,21 +4,23 @@ export default function Admin() {
     return (
         <div className='admin__container'>
             <h1 className='admin__title'>Adicionar Imagem</h1>
-
             <section className='admin__image-section'>
                 <div className='image-upload__container'>
-                    <input type='file' className='image-upload__uploader' />
+                    <input type='file' className='image-upload__uploader'
+                        onChange={() => {
+                            console.log('saas')
+                        }}
+                    />
                     <img src='' alt='' className='image-upload__preview' />
                 </div>
                 <div className='image-section__description'>
                     <h2 className='description__title'>Informações da imagem</h2>
-
                     <div className='description__container'>
-                        <label className='description__label' htmlFor='description'>Descrição</label>
+                        <label className='description__label sub-section__title' htmlFor='description'>Descrição</label>
                         <textarea id='description' rows={3} className='description__text' placeholder='Insira descrição aqui'></textarea>
                     </div>
 
-                    <h3 className='categorias__title'>Categorias</h3>
+                    <h3 className='categorias__title sub-section__title'>Categorias</h3>
                     <ul className='image-section__categorias'>
                         {listaCategorias.map((categoria, index) => (
                             <li key={index} className='categorias__item'>
@@ -27,11 +29,6 @@ export default function Admin() {
                             </li>
                         ))}
                     </ul>
-
-                    {/* <div>
-                        <h3>Adicione uma categoria</h3>
-                        <input />
-                    </div> */}
                 </div>
             </section>
         </div>
